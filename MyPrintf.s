0903d4d74f6a9b7f16b2b6db11d9edbc8acf9a89
Egor Kokonin <egorkokonin557@gmail.com>
2024-04-02 11:20:50 +0300

    add "%%"

--- MyPrintf.s
+++ MyPrintf.s
@@ -1,33 +1,79 @@
-;:================================================
+;:=============================================================================================================================
 ;: MyPrintf                  
-;:================================================
+;:=============================================================================================================================
 
 section .text
 
 global _start                  
 
-_start:    
-            mov rbx, 1                      ;index of args without first arg
+_start:         
+;fill the stack
+                push arg2
+                push arg1
+                push formatStr
+                
+                mov rbx, 0                       
+                mov r8, [rsp]                             
 
-scan_args:  inc rbx
-;we are looking for the length of the string with the program name
-            mov rdi, [rsp+8*rbx]                ;we will get in RDI the address of argument with number rbx
-            xor rax, rax                    
-            mov rcx, -1                     ;argument can be long, we put a very large number in rcx
-            repne scasb                     ;looking for a null byte        do{rdi++; } while(cx != 0 && al != [rdi])
-            mov byte [rdi-1], 0ah           ;0ah = '\n'
-            sub rdi, [rsp+8*rbx]                ;get line length   
+D:              mov rbp, r8
+                mov rcx, -1
+A:              cmp byte [rbp], '%'
+                je B
+                cmp byte [rbp], 0
+                je B
+                inc rbp
+                loop A                                 
 
-;output the line
-            mov rax, 0x1
-            mov rdx, rdi
-            mov rdi, 1
-            mov rsi, [rsp+8*rbx]
-            syscall
+B:              mov rsi, r8
+                mov rdx, rbp
+                sub rdx, r8
+                call print_string
+                mov r8, rbp
+                add r8, 2
 
-            cmp rbx, [rsp]
-            jne scan_args
+                cmp byte [rbp], 0
+                je Done
+
+E:              cmp byte [rbp+1], '%'
+                jne C
+
+                mov rsi, percent
+                mov rdx, 1
+                call print_string
+                jmp D
+
+C:              inc bx
+                mov rcx, -1
+                mov rdi, [rsp+8*rbx] 
+                xor rax, rax
+                repne scasb
+
+                sub rdi, [rsp+rbx*8]         
+                mov rdx, rdi
+                mov rsi, [rsp+rbx*8]
+                call print_string 
+                jmp D
+
 ; return 0
-            mov rax, 0x3C      
-            xor rdi, rdi
-            syscall
+Done:           mov rax, 0x3C      
+                xor rdi, rdi
+                syscall
+
+;---------------------------------------------------------------------------------------------------------------------------
+; Output a string to standard output
+; Entry: rsi - line address; rdx - line length
+; Destroy: rax, rdi	
+; Returns: -
+;---------------------------------------------------------------------------------------------------------------------------
+print_string:   mov rax, 0x1            
+                mov rdi, 1
+                syscall
+
+                ret
+
+section .data
+
+formatStr       db      "%%%", 0
+arg1            db      "Egorik", 0
+arg2            db      65
+percent         db      '%'
